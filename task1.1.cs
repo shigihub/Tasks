759af66165a71025aacd307633b819db65a786bc
shigihub <shirvani.abddullayev@gmail.com>
2019-01-23 10:42:07 +0100

    Create task1.1.cs

---
+++ task1.1.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace task1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i<21; i++) {
+
+                Console.WriteLine(i);
+                if (i == 13) {
+
+                    Console.WriteLine("This is my lucky number");
+                }
+
+            }
+        }
+    }
+}
